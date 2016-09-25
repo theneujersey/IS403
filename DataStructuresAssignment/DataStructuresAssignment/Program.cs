f4830073c1c2ecf775b9810b9a09bb0869f17189
theneujersey <jneu64@gmail.com>
2016-09-25 00:17:47 -0600

    Add files via upload

---
+++ DataStructuresAssignment/DataStructuresAssignment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iAnswer = 0;
+            do {
+                Console.WriteLine("Please enter an integer 1-4.");
+                Console.WriteLine("1. Stack\n2. Queue\n3. Dictionary\n4. Exit");
+                
+                Boolean bException = false;
+                while(bException == false) {
+                    try {
+                        string sAnswer = Console.ReadLine();
+                        iAnswer = Convert.ToInt32(sAnswer);
+                        bException = true;
+                    }
+                    catch {
+                        Console.WriteLine("Please enter an integer 1-4.");
+                        Console.WriteLine("1. Stack\n2. Queue\n3. Dictionary\n4. Exit");
+                    }
+                }
+            } while (iAnswer < 1 || iAnswer > 4);
+
+            switch (iAnswer) {
+                case 1:
+                    Console.WriteLine("Stack");
+                    break;
+                case 2:
+                    Console.WriteLine("Queue");
+                    break;
+                case 3:
+                    Console.WriteLine("Dictionary");
+                    break;
+                case 4:
+                    System.Environment.Exit(1);
+                    break;
+                default:
+                    Console.WriteLine("Please enter an integer 1-4.");
+                    Console.WriteLine("1. Stack\n2. Queue\n3. Dictionary\n4. Exit");
+                    break;
+            }
+
+            string sEnd = Console.ReadLine();
+        }
+    }
+}
