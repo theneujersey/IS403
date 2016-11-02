b3e5c8c7473caf1c4b85cc3dbbdee653c3f87015
theneujersey <jneu64@gmail.com>
2016-11-01 18:42:50 -0600

    Added BlowOut and .gitignore file

---
+++ BlowOut/BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {       
+        // GET: Contact
+        public String Index()
+        {           
+            return "Please call Support at <u><strong>801-555-1212</strong></u>. Thank you!";
+        }
+
+        public String Email(String Name, String Email)
+        {
+            return "Thank you " + Name + ". We will send an email to " + Email;
+        }
+    }
+}
---
+++ BlowOut/BlowOut/Controllers/RentalController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class RentalController : Controller
+    {
+        // GET: Rental
+        public ActionResult Rentals()
+        {
+            return View();
+        }
+
+        public ActionResult RentalDetails(string instrument, string condition)
+        {
+            switch (instrument)
+            {
+                case "Trumpet":
+                    ViewBag.instrument = "Trumpet";
+                    ViewBag.image = "trumpet.jpg";
+                    ViewBag.newPrice = "New Price: $55.00/month";
+                    ViewBag.usedPrice = "Used Price: $25.00/month";
+                    break;
+                case "Trombone":
+                    ViewBag.instrument = "Trombone";
+                    ViewBag.image = "trombone.jpg";
+                    ViewBag.newPrice = "New Price: $60.00/month";
+                    ViewBag.usedPrice = "Used Price: $35.00/month";
+                    break;
+                case "Tuba":
+                    ViewBag.instrument = "Tuba";
+                    ViewBag.image = "tuba.jpg";
+                    ViewBag.newPrice = "New Price: $70.00/month";
+                    ViewBag.usedPrice = "Used Price: $50.00/month";
+                    break;
+                case "Flute":
+                    ViewBag.instrument = "Flute";
+                    ViewBag.image = "flute.jpg";
+                    ViewBag.newPrice = "New Price: $40.00/month";
+                    ViewBag.usedPrice = "Used Price: $25.00/month";
+                    break;
+                case "Clarinet":
+                    ViewBag.instrument = "Clarinet";
+                    ViewBag.image = "clarinet.jpg";
+                    ViewBag.newPrice = "New Price: $35.00/month";
+                    ViewBag.usedPrice = "Used Price: $27.00/month";
+                    break;
+                case "Saxophone":
+                    ViewBag.instrument = "Saxophone";
+                    ViewBag.image = "saxophone.jpg";
+                    ViewBag.newPrice = "New Price: $42.00/month";
+                    ViewBag.usedPrice = "Used Price: $30.00/month";
+                    break;
+                default:
+                    ViewBag.instrument = "Chuck Norris";
+                    ViewBag.image = "RentToOwn.jpg";
+                    ViewBag.newPrice = "New Price: $1000.00/month";
+                    ViewBag.usedPrice = "Used Price: $999.00/month";
+                    break;
+            }
+
+            if (condition == "new")
+            {
+                ViewBag.price = ViewBag.newPrice;
+            }
+            else if (condition == "used")
+            {
+                ViewBag.price = ViewBag.usedPrice;
+            }
+
+            return View();
+        }
+    }
+}
